4eeacd88df15454529f16e41d38f068741f3a623
Diego <luis.quintana.g@tecsup.edu.pe>
2025-11-20 18:05:21 -0500

    ga

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    buildActivityPosition: 'bottom-right',
-  },
+  /* config options here */
 };
 
 export default nextConfig;
